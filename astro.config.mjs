192ae880e414ee582ce83adae8e216db9cf5ad5e
Facundo Personal <facundopellicer4@gmail.com>
2026-05-19 14:59:40 -0300

    add base styles, fonts and logos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -8,5 +8,39 @@ export default defineConfig({
   site: "https://box28012.com",
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Anton",
+        cssVariable: "--font-display",
+        weights: [400],
+        styles: ["normal"],
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.local(),
+        name: "Chulapa",
+        cssVariable: "--font-sans",
+        options: {
+          variants: [
+            {
+              weight: 400,
+              style: "normal",
+              src: ["./src/assets/fonts/chulapa/Chulapa-Regular.otf"],
+            },
+            {
+              weight: 700,
+              style: "normal",
+              src: ["./src/assets/fonts/chulapa/Chulapa-Bold.otf"],
+            },
+            {
+              weight: 300,
+              style: "normal",
+              src: ["./src/assets/fonts/chulapa/Chulapa-Light.otf"],
+            },
+          ],
+        },
+      },
+    ],
 });
