26f572b2434555e469c2e786d29c9401a7c84751
Facundo Personal <facundopellicer4@gmail.com>
2026-05-20 11:33:27 -0300

    initial structure and content

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,44 +3,58 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://box28012.com",
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Anton",
-        cssVariable: "--font-display",
-        weights: [400],
-        styles: ["normal"],
-        subsets: ["latin"],
+    {
+      provider: fontProviders.google(),
+      name: "Anton",
+      cssVariable: "--font-display",
+      weights: [400],
+      styles: ["normal"],
+      subsets: ["latin"],
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Chulapa",
+      cssVariable: "--font-sans",
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/chulapa/Chulapa-Regular.otf"],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/chulapa/Chulapa-Bold.otf"],
+          },
+          {
+            weight: 300,
+            style: "normal",
+            src: ["./src/assets/fonts/chulapa/Chulapa-Light.otf"],
+          },
+        ],
       },
-      {
-        provider: fontProviders.local(),
-        name: "Chulapa",
-        cssVariable: "--font-sans",
-        options: {
-          variants: [
-            {
-              weight: 400,
-              style: "normal",
-              src: ["./src/assets/fonts/chulapa/Chulapa-Regular.otf"],
-            },
-            {
-              weight: 700,
-              style: "normal",
-              src: ["./src/assets/fonts/chulapa/Chulapa-Bold.otf"],
-            },
-            {
-              weight: 300,
-              style: "normal",
-              src: ["./src/assets/fonts/chulapa/Chulapa-Light.otf"],
-            },
-          ],
-        },
-      },
-    ],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Alegreya Sans",
+      cssVariable: "--font-body",
+      weights: [300, 400, 500, 700, 800, 900],
+      styles: ["normal", "italic"],
+      subsets: ["latin"],
+    }
+  ],
+
+  integrations: [icon()]
 });
